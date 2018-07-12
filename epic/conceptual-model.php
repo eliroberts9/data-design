aad064ed994261a31a8266f46bc4b3a2b43c6d45
Elias Roberts <eliroberts9@gmail.com>
2018-07-12 16:12:53 -0600

    removed userRecent, userPosts, userComments removed commentDepth added commentParentId edited relationships for accuracy

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -16,9 +16,6 @@
 				<li>userHash</li>
 				<li>userJoinDate</li>
 				<li>userLastLogin</li>
-				<li>userRecent</li>
-				<li>userPosts</li>
-				<li>userComments</li>
 			</ul>
 			<br />
 
@@ -26,9 +23,9 @@
 			<ul>
 				<li>commentId (Primary Key)</li>
 				<li>commentUserId (Foreign Key)</li>
+				<li>commentParentId</li>
 				<li>commentContent</li>
 				<li>commentDate</li>
-				<li>commentDepth</li>
 			</ul>
 			<br />
 
@@ -49,10 +46,9 @@
 			<li>Relationships</li>
 			<ul>
 				<li>One USER can have many COMMENTS (1 to n).</li>
-				<li>One USER can have many RECOMMENDS (1 to n).</li>
-				<li>One USER can have many FLAGS. (1 to n).</li>
-				<li>One COMMENT can have many RECOMMENDS. (1 to n).</li>
-				<li>One COMMENT can have many FLAGS. (1 to n).</li>
+				<li>One COMMENT can have many COMMENTS (1 to n).</li>
+				<li>Many USERS can have many RECOMMENDS (m to n).</li>
+				<li>Many USERS can have many FLAGS. (m to n).</li>
 			</ul>
 		</ul>
 		<a href="./index.php">Home</a>
